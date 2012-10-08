89d3778a8c467f540793b8e32af1f842a9c44922
Zarathon Maia <zarathonmaia@gmail.com>
2012-10-08 15:56:26 -0300

    Inicial do Rails
    Sistema Sacu, versão 0.1 - Somente o Rails instalado.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sacu::Application.config.secret_token = 'd982c018743560b7f93e299e173adec6e5303a20de47dc1761ef0612a52fa6ae05ebdcaef9e96f595072521c286ad4e9839b95b65085bd87f62b3ee44eb9f111'
