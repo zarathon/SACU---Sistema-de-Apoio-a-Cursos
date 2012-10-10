f3b5cfe7bd04680af5424ba3a788c1d7618919a6
Zarathon Maia <zarathonmaia@gmail.com>
2012-10-10 01:27:09 -0300

    Validações
    Adicionado as validações do Curso e da Aula

--- app/models/aula.rb
+++ app/models/aula.rb
@@ -1,4 +1,6 @@
 class Aula < ActiveRecord::Base
   belongs_to :curso
   attr_accessible :conteudo, :resumo, :titulo, :curso_id
+  validates_presence_of :conteudo, :message => "nao pode ser branco."
+  validates_presence_of :titulo, :message => "nao pode ser branco."
 end
--- app/models/curso.rb
+++ app/models/curso.rb
@@ -4,6 +4,10 @@ class Curso < ActiveRecord::Base
   has_many :aluno, :through => :matriculas
   has_many :aulas
   
+  validates_presence_of :professor_id, :message => "nao pode ficar em branco."
+  validates_presence_of :nome, :message => "nao pode ficar em branco."
+  validates_presence_of :descricao, :message => "nao pode ficar em branco."
+  
   def to_s 
     self.nome
   end
